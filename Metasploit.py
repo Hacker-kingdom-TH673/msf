9d0d1055a253007eb7779507d6c3dfa6bccdc280
Hacker-kingdom-TH673 <58379849+Hacker-kingdom-TH673@users.noreply.github.com>
2019-12-07 20:40:33 +0700

    msf

---
+++ Metasploit.py
@@ -0,0 +1,40 @@
+#Metasploit
+#Python Programing By Hacker kingdom TH
+import os,sys
+
+os.system("clear")
+print
+print "\033[1m\033[32m Python Programing By Hacker kingdom TH"
+print
+print "\033[0m"
+print
+print ("[1] metasploit")
+print ("[2] msfgbd")
+
+options = input("=>")
+if options == 1:
+    os.system("clear")
+    print ("==>Hacking<==")
+    print ("are You Hack")
+    print "\033[1m\033[32m Python Programing By Hacker kingdom TH"
+    os.system("pkg install curl")
+    os.system("pkg install metasploit")
+    os.system("figlet Hacker kingdom TH |lolcat")
+    print
+    print "\033[1m\033[32m Python Programing By Hacker kingdom TH"
+    print
+    print "\033[0m"
+    print ("[1] metasploit")
+    print ("[2] msfgbd")
+   
+    meta = input("=>")
+    if meta == 2:
+        os.system("clear")
+        print
+        print
+        print "\033[1m\033[32m Python Programing By Hacker kingdom TH"
+        os.system("git clone https://github.com/Hacker-kingdom-TH673/metasploit")
+        print
+        print
+        print "\033[1m\033[32m Python Programing By Hacker kingdom TH"
+        os.system("figlet Hacker kingdom TH |lolcat")
---
+++ README.md
@@ -0,0 +1,22 @@
+#Scrip msf
+#Creator YouTube Hacker kingdom TH
+#Scrip will about Metasploit in Termux
+Installation Step :
+
+$pkg upgarde
+
+$pkg update
+
+$pkg install python
+
+$pkg install python2
+
+$pkg install git
+
+Using Step :
+
+$cd msf
+
+$python2 Metasploit.py
+
+Bye Bye
